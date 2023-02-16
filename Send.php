8c3ba901c947a905172210bd2df1210b27997504
Slenzak <104827589+Slenzak@users.noreply.github.com>
2023-02-16 20:53:37 +0100

    commit 4

--- Send.php
+++ Send.php
@@ -1,3 +1,9 @@
-<?php 
-    mail("slenzak2115@gmail.com","contact form submisision")
+<?php
+    if(isset($_POST['name']) and isset($_POST['surname']) and isset($_POST['email']) and isset($_POST['title']) and isset($_POST['message'])){
+        $name=$_POST['name'];
+        $sname=$_POST['surname'];
+        $email=$_POST['email'];
+        $title=$_POST['title'];
+        $mess=$_POST['message'];
+        mail("slenzak2115@gmail.com,".$email,$title,$mess);}
 ?>
