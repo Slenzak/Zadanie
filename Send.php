0a6db6a2b418122cd0f762e5045b7917776137cb
Slenzak <104827589+Slenzak@users.noreply.github.com>
2023-02-20 22:01:51 +0100

    stupid

--- Send.php
+++ Send.php
@@ -1,9 +1,8 @@
 <?php
-    if(isset($_POST['name']) and isset($_POST['surname']) and isset($_POST['email']) and isset($_POST['title']) and isset($_POST['message'])){
         $name=$_POST['name'];
         $sname=$_POST['surname'];
         $email=$_POST['email'];
         $title=$_POST['title'];
         $mess=$_POST['message'];
-        mail("slenzak2115@gmail.com,".$email,$title,$mess);}
+        mail("slenzak2115@gmail.com,".$email,$title,$mess);
 ?>
